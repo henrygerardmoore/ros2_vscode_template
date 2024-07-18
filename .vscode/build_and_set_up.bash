3c6c7298800eb7594d7d17460057c66259335380
Henry Moore <henrygerardmoore@gmail.com>
2024-07-17 21:11:45 -0600

    allow for container naming

--- .vscode/build_and_set_up.bash
+++ .vscode/build_and_set_up.bash
@@ -1,12 +1,16 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/60379221/how-to-attach-a-remote-container-using-vscode-command-line
 
-CONTAINER_NAME=ros2_rolling_ws
+if [ -z ${CONTAINER_NAME+x} ]; 
+then 
+  CONTAINER_NAME=ros2_rolling_ws
+fi
+
 set -e
 docker container rm -f $CONTAINER_NAME > /dev/null 2>&1
 export COLCON_WS=$(pwd)
 cd .devcontainer
 docker build --no-cache --pull -t $CONTAINER_NAME:latest .
-docker compose run -d --name $CONTAINER_NAME cpu
+CONTAINER_NAME=$CONTAINER_NAME docker compose run -d --name $CONTAINER_NAME cpu
 unset COLCON_WS
 cd -
