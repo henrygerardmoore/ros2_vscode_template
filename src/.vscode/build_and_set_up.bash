a04b790f7397754ea87f3869bc5ae7bd34b4a295
Henry Moore <henry.moore@picknik.ai>
2024-11-22 10:29:06 -0700

    allow cache

--- src/.vscode/build_and_set_up.bash
+++ src/.vscode/build_and_set_up.bash
@@ -12,7 +12,7 @@ echo "Building workspace $CONTAINER_NAME in $COLCON_WS..."
 
 set -e
 cd .devcontainer
-docker build --no-cache --pull -t $CONTAINER_NAME:latest .
+docker build --pull -t $CONTAINER_NAME:latest .
 docker container rm -f $CONTAINER_NAME > /dev/null 2>&1
 docker compose run -d --name $CONTAINER_NAME cpu
 unset COLCON_WS
